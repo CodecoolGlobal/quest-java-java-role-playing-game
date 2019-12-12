10d726577a1ed4708ec24ee71333943289d1dc1b
kadartibi <kadart78@gmail.com>
2019-12-12 13:28:54 +0100

    + thief aggro and movement implemented

--- src/main/java/com/codecool/quest/logic/actors/Player.java
+++ src/main/java/com/codecool/quest/logic/actors/Player.java
@@ -1,7 +1,6 @@
 package com.codecool.quest.logic.actors;
 
 import com.codecool.quest.logic.Cell;
-import com.codecool.quest.logic.GameMap;
 import com.codecool.quest.logic.items.Item;
 import com.codecool.quest.logic.items.Key;
 import com.codecool.quest.logic.items.Tool;
